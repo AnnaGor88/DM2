51664bd148709cc30b4e1b9faa7dd8f1cd70e307
Anna <goryachevaanny@gmail.com>
2021-02-17 19:02:32 +0300

    Initial Commit

---
+++ DM2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DM2
+//
+//  Created by Анна Горячева on 17.02.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
